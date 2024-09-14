6143eb9ea10e8bba93dcc905200364986d0f41fa
SaRai <pasinlao2546@gmail.com>
2024-09-14 21:53:45 +0700

    edit url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://127.0.0.1:5000');  // เชื่อมต่อไปยัง Flask
+const socket = io('http://localhost:5000');  // เชื่อมต่อไปยัง Flask
 
 function App() {
   const [message, setMessage] = useState('');
