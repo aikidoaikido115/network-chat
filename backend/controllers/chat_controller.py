c526b73e5194a69bd41e386d2d84d98c1d4c42ea
SaRai <pasinlao2546@gmail.com>
2024-09-21 18:21:34 +0700

    test

--- backend/controllers/chat_controller.py
+++ backend/controllers/chat_controller.py
@@ -1,13 +1,5 @@
-from flask_socketio import emit
+from flask_socketio import send
 
-def handle_data(data):
-    message = data.get('message')
-    image = data.get('image')
-    
-    # ประมวลผลข้อความและรูปภาพตามต้องการ
-    if message:
-        emit('message', message)  # ส่งข้อความกลับไปยัง Client
-    if image:
-        # คุณสามารถจัดการกับข้อมูลรูปภาพที่นี่ได้
-        print("Received image data:", image)
-
+def handle_message(msg):
+    print(f"Message: {msg}")
+    send(msg, broadcast=True)
