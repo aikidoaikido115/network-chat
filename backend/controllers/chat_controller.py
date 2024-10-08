49d1b1b94f4b39affee3b2e9e06d60f58730f828
Becham-chan <111549621+Becham-chan@users.noreply.github.com>
2024-10-08 21:50:35 +0700

    username display

--- backend/controllers/chat_controller.py
+++ backend/controllers/chat_controller.py
@@ -1,5 +1,13 @@
-from flask_socketio import send
+from flask_socketio import send, emit, SocketIO
+from flask import jsonify
+import json
 
-def handle_message(msg):
-    print(f"Message: {msg}")
-    send(msg, broadcast=True)
+def handle_message(msg_json):
+    # แปลง JSON ให้ดึงค่าได้เพื่อแสดงผล
+    json_load = json.loads(msg_json)
+    message, username = json_load["msg"], json_load["username"]
+    # Handle incoming message from client
+    print(f"Message: {message} from {username}")
+
+    # print(f"Message: {msg}")
+    send(msg_json, broadcast=True)
